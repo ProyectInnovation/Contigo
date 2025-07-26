3987761fd31b65da70007fc2fbd0284f55c6aef4
ProyectInnovation <proyectinnovtion@gmail.com>
2025-07-26 13:07:42 -0600

    Add files via upload

--- chatbotAI.php
+++ chatbotAI.php
@@ -228,6 +228,17 @@
         draw();
     </script>
 
+    <script>
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('service-worker.js')
+                .then(function (registration) {
+                    console.log('Service Worker registrado con éxito:', registration);
+                })
+                .catch(function (error) {
+                    console.log('Fallo al registrar el Service Worker:', error);
+                });
+        }
+    </script>
 </body>
 
 </html>
--- geolocalizacion.php
+++ geolocalizacion.php
@@ -15,61 +15,61 @@ $psicologico = obtenerOrganismos($conn, 'organismos_psicologicos');
 <html lang="en">
 
 <head>
-  <!-- basic -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- mobile metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-  <!-- site metas -->
-  <title>Inicio</title>
-  <meta name="keywords" content="">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <!-- bootstrap css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <!-- style css -->
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <!-- Responsive-->
-  <link rel="stylesheet" href="css/responsive.css">
-  <!-- fevicon -->
-  <link rel="icon" href="images/fevicon.png" type="image/gif" />
-  <!-- Scrollbar Custom CSS -->
-  <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-  <!-- Tweaks for older IEs-->
-  <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-  <!-- owl stylesheets -->
-  <link rel="stylesheet" href="css/owl.carousel.min.css">
-  <link rel="stylesoeet" href="css/owl.theme.default.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
-    media="screen">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+   <!-- basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- mobile metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+   <!-- site metas -->
+   <title>Inicio</title>
+   <meta name="keywords" content="">
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <!-- bootstrap css -->
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <!-- style css -->
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+   <!-- Responsive-->
+   <link rel="stylesheet" href="css/responsive.css">
+   <!-- fevicon -->
+   <link rel="icon" href="images/fevicon.png" type="image/gif" />
+   <!-- Scrollbar Custom CSS -->
+   <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+   <!-- Tweaks for older IEs-->
+   <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+   <!-- owl stylesheets -->
+   <link rel="stylesheet" href="css/owl.carousel.min.css">
+   <link rel="stylesoeet" href="css/owl.theme.default.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
+      media="screen">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 </head>
 
 <body>
    <canvas id="orangeCanvas"></canvas>
-  <!-- header section start -->
-  <div class="header_section">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="logo custom-logo">
-        <a href="index.html"><img src="images/log.png" alt="Logo"></a>
-      </div>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link" href="index.html"><i class="fas fa-home"></i> Inicio</a>
-          <a class="nav-item nav-link" href="acerca.html"><i class="fas fa-info-circle"></i> Acerca de</a>
-          <a class="nav-item nav-link" href="informacion.html"><i class="fas fa-book"></i> Información</a>
-          <a class="nav-item nav-link" href="contact.php"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a>
-        </div>
-      </div>
-    </nav>
-  </div>
-  <!-- header section end -->
+   <!-- header section start -->
+   <div class="header_section">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <div class="logo custom-logo">
+            <a href="index.html"><img src="images/log.png" alt="Logo"></a>
+         </div>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+               <a class="nav-item nav-link" href="index.html"><i class="fas fa-home"></i> Inicio</a>
+               <a class="nav-item nav-link" href="acerca.html"><i class="fas fa-info-circle"></i> Acerca de</a>
+               <a class="nav-item nav-link" href="informacion.html"><i class="fas fa-book"></i> Información</a>
+               <a class="nav-item nav-link" href="contact.php"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a>
+            </div>
+         </div>
+      </nav>
+   </div>
+   <!-- header section end -->
 
 
    <!-- Choose Section: Organismos y Mapa -->
@@ -151,18 +151,18 @@ $psicologico = obtenerOrganismos($conn, 'organismos_psicologicos');
    </div>
    <!-- footer section end -->
 
-<!-- Javascript files-->
-  <script src="js/jquery.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.bundle.min.js"></script>
-  <script src="js/jquery-3.0.0.min.js"></script>
-  <script src="js/plugin.js"></script>
-  <!-- sidebar -->
-  <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-  <script src="js/custom.js"></script>
-  <!-- javascript -->
-  <script src="js/owl.carousel.js"></script>
-  <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+   <!-- Javascript files-->
+   <script src="js/jquery.min.js"></script>
+   <script src="js/popper.min.js"></script>
+   <script src="js/bootstrap.bundle.min.js"></script>
+   <script src="js/jquery-3.0.0.min.js"></script>
+   <script src="js/plugin.js"></script>
+   <!-- sidebar -->
+   <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+   <script src="js/custom.js"></script>
+   <!-- javascript -->
+   <script src="js/owl.carousel.js"></script>
+   <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 
    <!-- Google Maps JS -->
    <script async
@@ -315,6 +315,18 @@ $psicologico = obtenerOrganismos($conn, 'organismos_psicologicos');
       resize();
       createBubbles();
       draw();
+   </script>
+
+   <script>
+      if ('serviceWorker' in navigator) {
+         navigator.serviceWorker.register('service-worker.js')
+            .then(function (registration) {
+               console.log('Service Worker registrado con éxito:', registration);
+            })
+            .catch(function (error) {
+               console.log('Fallo al registrar el Service Worker:', error);
+            });
+      }
    </script>
 </body>
 
